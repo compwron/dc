f8125e641cf1b83d27f875f55bc4948c9736169b
CW <compiledwrong+github@gmail.com>
2020-09-30 17:58:10 -0700

    wip

---
+++ test.sql
@@ -0,0 +1,7 @@
+SELECT core_user.email, count(*) AS c
+FROM core_user
+         JOIN core_usermailing ON core_user.id = core_usermailing.user_id
+         left outer JOIN core_open ON (core_user.id = core_open.user_id AND core_usermailing.mailing_id = core_open.mailing_id)
+GROUP BY core_user.id, core_open.id
+ORDER BY c DESC
+LIMIT 10
