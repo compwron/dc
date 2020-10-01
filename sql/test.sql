63dc21badcedcf8ddba0e7163da02614671bf57e
CW <compiledwrong+github@gmail.com>
2020-10-01 15:54:26 -0700

    remove test

--- sql/test.sql
+++
@@ -1,7 +0,0 @@
-SELECT core_user.email, count(*) AS c
-FROM core_user
-         JOIN core_usermailing ON core_user.id = core_usermailing.user_id
-         left outer JOIN core_open ON (core_user.id = core_open.user_id AND core_usermailing.mailing_id = core_open.mailing_id)
-GROUP BY core_user.id, core_open.id
-ORDER BY c DESC
-LIMIT 10
