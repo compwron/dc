921b5cf1f75b59d1d79c13d6fddcba2aad8c8e26
CW <compiledwrong+github@gmail.com>
2020-10-01 15:38:08 -0700

    email_click_rate_per_member.sql

--- email_click_rate_per_member.sql
+++ email_click_rate_per_member.sql
@@ -1,8 +1,41 @@
-select core_user.email, core_usermailing.mailing_id, count(*) as c
-from core_click
-         join core_usermailing on core_click.user_id = core_usermailing.user_id
-         join core_user on core_click.user_id = core_user.id
-where core_click.created_at > '2020-01-01'
-group by core_user.email, core_usermailing.mailing_id
-order by core_usermailing.mailing_id desc
-limit 100
+select email,
+       user_id,
+       mailer_count,
+       open_count,
+       open_count / mailer_count   as avg_opens_per_mailer,
+       click_count,
+       click_count / mailer_count  as avg_clicks_per_mailer,
+       action_count,
+       action_count / mailer_count as avg_actions_per_mailer
+from (
+         select email,
+                user_id,
+                (select count(*)
+                 from core_usermailing
+                 where core_usermailing.user_id = user_mailers.user_id
+                ) as mailer_count,
+                (
+                    select count(*)
+                    from core_open
+                    where core_open.user_id = user_mailers.user_id
+                ) as open_count,
+                (
+                    select count(*)
+                    from core_click
+                    where core_click.user_id = user_mailers.user_id
+                ) as click_count,
+                (
+                    select count(*)
+                    from core_action
+                    where core_action.user_id = user_mailers.user_id
+                ) as action_count
+         from (
+                  select core_user.email as email,
+                         core_user.id    as user_id,
+                         count(*)        as mailer_count
+                  from core_usermailing
+                           join core_user on core_usermailing.user_id = core_user.id
+                  group by core_user.email
+              ) as user_mailers
+     ) as open_click_action
+
